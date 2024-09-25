a4c318e073c2a7b0847c9ff2ccd9b00efb228b1f
Alena-Sergeeva <sergeevanat71@icloud.com>
2024-09-25 17:20:59 +0300

    Убрала printf по возможности из функций

--- t_1.c
+++ t_1.c
@@ -2,89 +2,131 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
+
+enum err{
+    OK,
+    NOT_NUM,
+    WRONG_FLAG,
+    NUM_TOO_BIG,
+    ARGUMETS,
+    NOT_SUIT,
+    ONE
+};
+
+
 /*-h вывести в консоль натуральные числа в пределах 100 включительно, кратные x.
 Если таковых нету – вывести соответствующее сообщение */
-void f_h(long int x)
+int multiples(long int x);
+int prime_numb(long int x, int * pr_num);
+int base_16(long int x, char * num_16);
+int pw_table(long int x);
+int sum_natur(long int x, long int *res);
+int fact(long int x, long int *res);
+
+void len(long int x, int *i);
+int valid(int argc, char * argv[], long int * pt_x, char * fl);
+
+int main(int argc, char * argv[])
 {
-    int i = 2;
-    while (x <= 100)
+    long int x = 0, res = 0;
+    char fl = ' ';
+    enum err f_mistake = 0;
+    switch (valid(argc, argv, &x, &fl))
     {
-        printf("%ld ", x);
-        x *= i;
-        ++i;
+        case OK:
+            switch (fl)
+            {
+                case 'h':
+                    f_mistake = multiples(x);
+                    if (f_mistake == NOT_SUIT)
+                    {
+                        printf("x должно быть положительным, чтобы вывести кратные ему числа\n");
+                    }
+
+                    break;
+                case 'p':
+                    int pr_num = 0;
+                    f_mistake = prime_numb(x, &pr_num);
+                    if (f_mistake == NOT_SUIT)
+                    {
+                         printf("Число x неположительное, нельзя определить простое оно или составное\n");
+                    }
+                    if (f_mistake == ONE)
+                    {
+                         printf("Не является ни простым ни сотавным\n");
+                    }
+                    if (f_mistake == OK)
+                    {
+                        if (pr_num == 0)
+                        {
+                            printf("Число составное\n");
+                        }
+                        else
+                        {
+                            printf("Число простое\n");
+                        }
+                    }
+
+
+                    break;
+                case 's':
+                    char num_16[sizeof(long int) * 8 + 1];
+                    base_16(x, num_16);
+
+
+                    for (int i = strlen(num_16)-1; i >= 0; --i)
+                    {
+                        printf("%c ", num_16[i]);
+                    }
+                    printf("\n");
+                    break;
+                case 'e':
+                    if (pw_table(x) == NOT_SUIT)
+                        printf("Число x неположительное\n");
+                    break;
+                case 'a':
+                    if (sum_natur(x, &res) == NOT_SUIT)
+                    {
+                        printf("Число x меньше 1. Сумма натуральных чисел до x равна 1\n");
+                    }
+                    else
+                    {
+                        printf("%ld\n", res);
+                    }
+                    break;
+                case 'f':
+                    f_mistake = fact(x, &res);
+                    if (f_mistake == NOT_SUIT)
+                    {
+                        printf("Нельзя вычислить факториал от отрицательного числа\n");
+                    }
+                    if (f_mistake == NUM_TOO_BIG)
+                    {
+                        printf("При вычислении произошло переполнение типа long int\n");
+                    }
+                    if (f_mistake == 0)
+                    {
+                        printf("%ld\n", res);
+                    }
+                    break;
+            }
+            break;
+        case WRONG_FLAG:
+            printf("Флаг введен не верно\n");
+            break;
+        case NOT_NUM:
+            printf("Строка не вляется десятичным целым числом\n");
+            break;
+        case NUM_TOO_BIG:
+            printf("переполнение типа long int\n");
+            break;
+        case ARGUMETS:
+            printf("Введено неверное колличество аргументов\n");
+            break;
+
     }
-    if (i == 2)
-        printf("Чисел кратных x в пределах 100 включительно нет");
-    printf("\n");
-}
 
-// -p определить, является ли число x простым; является ли x составным
-void f_p(long int x)
-{
-    int i=2;
-    if (x < 0)
-    {
-        printf("Отрицательное число не может быть простым");
-        return;
-    }
-    for (i; (i <= x / 2) && (x % i != 0); ++i){};
-    /*if (x % i == 0)
-        printf("x составное число")
-    else printf("x простое число");*/
-    ((x % i == 0) || (x == 1)) ? printf("x составное число\n") : printf("x простое число\n");
-}
-
-/*разделить число x на отдельные цифры системы счисления с основанием 16 и
-вывести отдельно каждую цифру числа, разделяя их пробелом, от старших
-разрядов к младшим, без ведущих нулей в строковом представлении*/
-void ch(long int x)
-{
-    if (x == 0)
-        return;
-    ch(x / 16);
-    int y = x % 16;
-
-    printf("%c ", (y < 10) ? ('0' + y) : ('A' + y % 10));
-}
-
-void f_s(long int x)
-{
-    (x == 0) ? (printf("0")) : ch(abs(x));
-    printf("\n");
-}
-
-/*????-e вывести таблицу степеней (для всех показателей в диапазоне от 1 до x)
-оснований от 1 до 10; для этого флага работает ограничение на вводимое число: x
-должен быть не больше 10*/
-
-
-/*
-void f_e(long int x)
-{
-
-}
-*/
-
-//-a вычислить сумму всех натуральных чисел от 1 до x и вывести полученное
-//значение в консоль
-
-
-// что делать с отрицательными числами ???????????
-
-void f_a(long int x)
-{
-    printf("%ld", (1 + x) / 2 * x);
-}
-
-//-f вычислить факториал x и вывести полученное значение в консоль
-void f_f(long int x)
-{
-    long long int f = 1;
-    for (int i = 2; i <= x; ++i)
-    {
-        f *= i;
-    }
-    printf("%lld\n", f);
+    return 0;
 }
 
 void len(long int x, int *i)
@@ -99,66 +141,150 @@ void len(long int x, int *i)
         ++(*i);
     }
 }
-int valid(int argc, char * argv[], long int * pt_x)
+int valid(int argc, char * argv[], long int * pt_x, char * fl)
 {
     if (argc != 3)
     {
-        printf("Введено неверное количество аргументов\n");
-        return 0;
-    }
-    //нужно ли проверять корректность введения названия программы argv[0]
-    * pt_x = strtol(argv[1],'\0', 10);
-    int len_x = 0;
-    len(* pt_x, &len_x);
-    if ((len_x != strlen(argv[1])) || (*pt_x == 0 && strcmp(argv[1], "0") != 0))
-    {
-        printf("Строка не вляется десятичным целым числом\n");
-        return 0;
+        return ARGUMETS;
     }
 
-    //printf("%ld %d\n", *pt_x, len_x);
+    char * end_num = NULL;
+    * pt_x = strtol(argv[1], &end_num, 10);
+    int len_x = 0;
+    len(* pt_x, &len_x);
 
     if ((*pt_x == LONG_MAX) || (*pt_x == LONG_MIN))
     {
-        printf("переполнение типа long int\n");
-        return 0;
+        return NUM_TOO_BIG;
     }
 
-    if (!((argv[2][0] == '-' || argv[2][0] == '/') && strstr("a h p s f e", argv[2]+1)))
+    if ((len_x != strlen(argv[1])) || (*pt_x == 0 && strcmp(argv[1], "0") != 0))
     {
-        printf("Флаг введен не верно\n");
-        printf("%s", argv[2]+1);
-        return 0;
+        return NOT_NUM;
     }
-    if ((strcmp(argv[2], "-e") == 0) && (*pt_x > 10))
+
+    if (!((argv[2][0] == '-' || argv[2][0] == '/') && strstr(" a h p s f e ", argv[2]+1) && (strlen(argv[2])== 2)))
     {
-        printf("x должен быть не больше 10\n");
-        return 0;
+        return WRONG_FLAG;
     }
+    if ((argv[2][1] == 'e') && (*pt_x > 10))
+    {
+        return NOT_SUIT;
+    }
+    *fl = argv[2][1];
+    return 0;
 }
 
 
-int main(int argc, char * argv[])
+
+int multiples(long int x)
 {
-    long int x = 0;
+    if (x <= 0)
+    {
+        return NOT_SUIT;
+    }
+    int i = 0;
+    while (x * (i+1) <= 100)
+    {
+        ++i;
+        printf("%ld ", x * i);
+    }
+    if (i == 0)
+        printf("Чисел кратных x в пределах 100 включительно нет");
+    printf("\n");
+    return 0;
+}
 
-    if (!valid(argc, argv, &x))
-        return 1;
+// -p определить, является ли число x простым; является ли x составным
+int prime_numb(long int x, int * pr_num)
+{
+    if (x <= 0)
+    {
+        return NOT_SUIT;
+    }
+    if (x == 1)
+    {
+        return ONE;
+    }
+    int i=2;
+    for (i; (i <= x / 2) && (x % i != 0); ++i){};
+    (x % i == 0) ? ((*pr_num) = 0) : ((*pr_num) = 1);
+    return 0;
+}
 
-    if (&x == NULL)
-        return 1;
-    if (strcmp(argv[2], "-h") == 0)
-        f_h(x);
-    if (strcmp(argv[2], "-p") == 0)
-        f_p(x);
-    if (strcmp(argv[2], "-s") == 0)
-        f_s(x);
-    /*if (strcmp(argv[2], "-e") == 0)
-        f_e(x);*/
-    if (strcmp(argv[2], "-a") == 0)
-        f_a(x);
-    if (strcmp(argv[2], "-f") == 0)
-        f_f(x);
+/*разделить число x на отдельные цифры системы счисления с основанием 16 и
+вывести отдельно каждую цифру числа, разделяя их пробелом, от старших
+разрядов к младшим, без ведущих нулей в строковом представлении*/
+int base_16(long int x, char * num_16)
+{
+    if (x == 0)
+    {
+        num_16[0] = '0';
+        return 0;
+    }
+    int i = 0, y = 0;
+    int n = abs(x);
+    while (n > 0)
+    {
+        y = n % 16;
+        num_16[i] = (y < 10) ? ('0' + y) : ('A' + y % 10);
+        n /= 16;
+        ++i;
+    }
+    num_16[i]='\0';
+    return 0;
+}
 
+/*????-e вывести таблицу степеней (для всех показателей в диапазоне от 1 до x)
+оснований от 1 до 10; для этого флага работает ограничение на вводимое число: x
+должен быть не больше 10*/
+int pw_table(long int x)
+{
+    if (x <= 0)
+    {
+        return NOT_SUIT;
+    }
+    int i, j;
+    for (i= 1; i <=10; ++i)
+    {
+        long int res = 1;
+        for (j = 1; j <= x; ++j)
+        {
+            res *= i;
+            printf("%11ld ", res);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//-a вычислить сумму всех натуральных чисел от 1 до x и вывести полученное
+//значение в консоль
+int sum_natur(long int x, long int *res)
+{
+    if (x <= 0)
+    {
+        return NOT_SUIT;
+    }
+    (*res) = ((1 + x) * x)/2;
+    return 0;
+}
+
+//-f вычислить факториал x и вывести полученное значение в консоль
+int fact(long int x, long int *res)
+{
+    if (x < 0)
+    {
+        return NOT_SUIT;
+    }
+    (*res) = 1;
+    int i = 2;
+    while ((i <= x) && ((*res) * i > (*res)))
+    {
+        (*res) *= i;
+        ++i;
+    }
+    if (i != x)
+        return NUM_TOO_BIG;
     return 0;
 }
