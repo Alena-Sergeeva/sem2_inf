acb7f4232adf5362b6d1014538d6bbd9efebb84d
Alena-Sergeeva <sergeevanat71@icloud.com>
2024-09-29 13:50:46 +0300

    пункт с. d не понимаю.

--- t_5.c
+++ t_5.c
@@ -4,96 +4,83 @@
 #include <math.h>
 #include <tgmath.h>
 
-enum err{
+enum err
+{
     OK,
     CNT_ARGUMENTS,
     NOT_NUM,
     NUM_TOO_BIG,
     WRONGE_EPS_VALUE,
-    SUM_TOO_BIG
+    SUM_TOO_BIG,
+    SERIES_DIVERGES
 };
-int valid (int, char * [], long double *, long double *, char *);
+int valid(int, char *[], long double *, long double *, char *);
 
 int sum_a(long double, long double, long double *);
 int sum_b(long double, long double, long double *);
 int sum_c(long double, long double, long double *);
 int sum_d(long double, long double, long double *);
 
-int main (int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     long double eps = 0.0, x = 0.0, res = 0.0;
     char fl = ' ';
     enum err mistake = 0;
     switch (valid(argc, argv, &x, &eps, &fl))
     {
-        case OK:
-            printf("%Lf, %Lf\n", eps, x);
-            mistake = sum_a(x, eps, &res);
-            if (mistake == SUM_TOO_BIG)
-            {
-                printf("a. Необходимая точность не достигнута, произошло переполнение\n");
-            }
-            else
-            {
-                printf("a. %.15Lf\n", res);
-            }
-            mistake = sum_b(x, eps, &res);
-            if (mistake == SUM_TOO_BIG)
-            {
-                printf("b. Необходимая точность не достигнута, произошло переполнение\n");
-            }
-            else
-            {
-                printf("b. %.15Lf\n", res);
-            }
-            mistake = sum_c(x, eps, &res);
-            if (mistake == SUM_TOO_BIG)
-            {
-                printf("c. Необходимая точность не достигнута, произошло переполнение\n");
-            }
-            else
-            {
-                printf("c. %.15Lf\n", res);
-            }
-            mistake = sum_d(x, eps, &res);
-            if (mistake == SUM_TOO_BIG)
-            {
-                printf("d. Необходимая точность не достигнута, произошло переполнение\n");
-            }
-            else
-            {
-                printf("d. %.15Lf\n", res);
-            }
-            break;
+    case OK:
+        printf("%Lf, %Lf\n", eps, x);
+        mistake = sum_a(x, eps, &res);
+        printf("a. %.15Lf\n", res);
+        mistake = sum_b(x, eps, &res);
+        printf("b. %.15Lf\n", res);
+        mistake = sum_c(x, eps, &res);
+        if (mistake == SERIES_DIVERGES)
+        {
+            printf("с. Ряд рассходится\n");
+        }
+        else
+        {
+            printf("c. %.15Lf\n", res);
+        }
+        mistake = sum_d(x, eps, &res);
+        if (mistake == SUM_TOO_BIG)
+        {
+            printf("d. Необходимая точность не достигнута, произошло переполнение\n");
+        }
+        else
+        {
+            printf("d. %.15Lf\n", res);
+        }
+        break;
 
-        case CNT_ARGUMENTS:
-            printf("Неверное количество аргументов командной строки\n");
-            break;
+    case CNT_ARGUMENTS:
+        printf("Неверное количество аргументов командной строки\n");
+        break;
 
-        case NOT_NUM:
-            printf("Аргумент не является числом, либо произошла потяря значимости\n");
-            break;
+    case NOT_NUM:
+        printf("Аргумент не является числом, либо произошла потяря значимости\n");
+        break;
 
-        case NUM_TOO_BIG:
-            printf("Произошло переполнение типа long double\n");
-            break;
+    case NUM_TOO_BIG:
+        printf("Произошло переполнение типа long double\n");
+        break;
 
-        case WRONGE_EPS_VALUE:
-            printf("eps -- должно быть положительным\n");
-            break;
-
+    case WRONGE_EPS_VALUE:
+        printf("eps -- должно быть положительным\n");
+        break;
     }
     return 0;
 }
 
-int valid (int argc, char * argv[], long double *x, long double * eps, char *fl)
+int valid(int argc, char *argv[], long double *x, long double *eps, char *fl)
 {
     if (argc != 3)
     {
         return CNT_ARGUMENTS;
     }
-    char * pt_stop_symbol = NULL;
-    * eps = strtold(argv[1], &pt_stop_symbol);
+    char *pt_stop_symbol = NULL;
+    *eps = strtold(argv[1], &pt_stop_symbol);
     if ((*eps == HUGE_VALL) || (*eps == (-HUGE_VALL)))
     {
         return NUM_TOO_BIG;
@@ -116,9 +103,8 @@ int valid (int argc, char * argv[], long double *x, long double * eps, char *fl)
         return WRONGE_EPS_VALUE;
     }
 
-
     pt_stop_symbol = NULL;
-    * x = strtold(argv[2], &pt_stop_symbol);
+    *x = strtold(argv[2], &pt_stop_symbol);
     if ((*x == HUGE_VALL) || (*x == (-HUGE_VALL)))
     {
         return NUM_TOO_BIG;
@@ -135,7 +121,7 @@ int valid (int argc, char * argv[], long double *x, long double * eps, char *fl)
 
     return 0;
 }
-//e^x
+// e^x
 int sum_a(long double x, long double eps, long double *res)
 {
     long double elem_n = 1;
@@ -143,10 +129,10 @@ int sum_a(long double x, long double eps, long double *res)
     *res = 1;
     while ((fabs(elem_n) > eps))
     {
-        elem_n *= (x / (n+1));
+        elem_n *= (x / (n + 1));
         (*res) += elem_n;
         ++n;
-        //printf("%Lf\n", *res);
+        // printf("%Lf\n", *res);
     }
     (*res) += elem_n;
     if (isinf(elem_n))
@@ -154,7 +140,7 @@ int sum_a(long double x, long double eps, long double *res)
 
     return 0;
 }
-//cosx
+// cosx
 int sum_b(long double x, long double eps, long double *res)
 {
     long double elem_n = 1;
@@ -162,10 +148,10 @@ int sum_b(long double x, long double eps, long double *res)
     *res = 1;
     while ((fabs(elem_n) > eps))
     {
-        elem_n *= (((-1) * x * x)/((2 * n + 2) * (2 * n + 1)));
+        elem_n *= (((-1) * x * x) / ((2 * n + 2) * (2 * n + 1)));
         (*res) = (*res) + elem_n;
         ++n;
-        //printf("%Lf\n", *res);
+        // printf("%Lf\n", *res);
     }
     (*res) += elem_n;
     if (isinf(*res))
@@ -179,12 +165,15 @@ int sum_c(long double x, long double eps, long double *res)
 {
     long double elem_n = 1;
     int n = 0;
+    if (fabs(x * x) >= 1)
+    {
+        return SERIES_DIVERGES;
+    }
     while ((fabs(elem_n) > eps) && (!isinf(*res)) && (!isnanl(*res)))
     {
-        elem_n *= ((9 * (n + 1) * (n + 1) * x * x)/((3 * n + 2)* (3 * n + 1)));
+        elem_n *= ((9 * (n + 1) * (n + 1) * x * x) / ((3 * n + 2) * (3 * n + 1)));
         ++n;
-        (*res)+=elem_n;
-        //printf("%LE\n", *res);
+        (*res) += elem_n;
     }
     (*res) += elem_n;
     if ((isinf(*res)) || (isnanl(*res)))
@@ -193,6 +182,7 @@ int sum_c(long double x, long double eps, long double *res)
     return 0;
 }
 
+// не понимаю
 int sum_d(long double x, long double eps, long double *res)
 {
     long double elem_n = ((-1) * (x * x)) / 2;
@@ -200,16 +190,15 @@ int sum_d(long double x, long double eps, long double *res)
 
     while ((fabs(elem_n) > eps) && (!isinf(*res)) && (!isnanl(*res)))
     {
-        elem_n *= ((x * x * (-1) * (n + 0.5))/ (n+1)) ;
+        elem_n *= ((x * x * (-1) * (2 * n + 1)) / (2 * n + 2));
         ++n;
         (*res) += elem_n;
-        //printf("%Lf\n", *res);
+        // printf("%Lf\n", *res);
     }
-    (* res) += elem_n;
+    (*res) += elem_n;
 
     if ((isinf(*res)) || (isnanl(*res)))
         return SUM_TOO_BIG;
 
     return 0;
 }
-
